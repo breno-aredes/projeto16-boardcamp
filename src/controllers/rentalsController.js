9d7cb1d8c7985fde37fc839be048019788defbcc
breno-aredes <breno_mg10@hotmail.com>
2023-02-11 12:24:58 -0300

    fix: base controllers

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -1,3 +1,9 @@
 import { db } from "../database/database.connection.js";
 
 export async function listRentals(req, res) {}
+
+export async function newRentals(req, res) {}
+
+export async function attRentals(req, res) {}
+
+export async function deleteRentals(req, res) {}
