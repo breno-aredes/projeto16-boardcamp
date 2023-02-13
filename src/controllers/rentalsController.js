fd00181e3eee1c720f68390821be0b8773cbc17e
breno-aredes <breno_mg10@hotmail.com>
2023-02-12 22:48:27 -0300

    feat: list rentals

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -1,6 +1,13 @@
 import { db } from "../database/database.connection.js";
 
-export async function listRentals(req, res) {}
+export async function listRentals(req, res) {
+  try {
+    const rentals = await db.query("SELECT * FROM rentals;");
+    res.send(rentals.rows);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
 
 export async function newRentals(req, res) {}
 
