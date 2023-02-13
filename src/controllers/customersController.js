a51204f8f816c10b3d99efb9211e79eabc2cb2b5
breno-aredes <breno_mg10@hotmail.com>
2023-02-12 22:43:48 -0300

    feat: list costumers

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -1,6 +1,13 @@
 import { db } from "../database/database.connection.js";
 
-export async function listCustomers(req, res) {}
+export async function listCustomers(req, res) {
+  try {
+    const costumers = await db.query("SELECT * FROM costumers;");
+    res.send(costumers.rows);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
 
 export async function newCustomers(req, res) {}
 
