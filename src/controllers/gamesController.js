ecabcba650c7113badd4a34f65d5807ce2db49da
breno-aredes <breno_mg10@hotmail.com>
2023-02-12 22:22:22 -0300

    feat: insert game created

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -9,4 +9,15 @@ export async function listGames(req, res) {
   }
 }
 
-export async function newGames(req, res) {}
+export async function newGames(req, res) {
+  const { name, image, stockTotal, pricePerDay } = req.body;
+  try {
+    await db.query(
+      `INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1,$2,$3,$4);`,
+      [name, image, stockTotal, pricePerDay]
+    );
+    res.sendStatus(201);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
