e72637443f294c398dd7c82b7044a91bfabd7c57
breno-aredes <breno_mg10@hotmail.com>
2023-02-13 17:03:17 -0300

    fix: rentals validation

--- src/middlewares/rentalsValidation.js
+++ src/middlewares/rentalsValidation.js
@@ -3,12 +3,12 @@ import { db } from "../database/database.connection.js";
 export async function rentalsValidate(req, res, next) {
   const { customerId, gameId } = req.body;
   try {
-    const game = await db.query(`SELECT * FROM games WHERE id = $1`, [gameId]);
-    const customer = await db.query(`SELECT * FROM customers WHERE id = $1`, [
+    const game = await db.query(`SELECT * FROM games WHERE id=$1`, [gameId]);
+    const customer = await db.query(`SELECT * FROM customers WHERE id=$1`, [
       customerId,
     ]);
     const rentedGames = await db.query(
-      'SELECT * FROM rentals JOIN games ON rentals."gameId" = games.id WHERE games.id = $1 AND rentals."returnDate" IS NULL;',
+      'SELECT * FROM rentals JOIN games ON rentals."gameId" = games.id WHERE games.id=$1 AND rentals."returnDate" IS NULL;',
       [gameId]
     );
 
@@ -25,9 +25,8 @@ export async function rentalsValidate(req, res, next) {
 export async function rentalsAttValidate(req, res, next) {
   const { id } = req.params;
   try {
-    const rental = await db.query('SELECT * FROM rentals WHERE "id" = $1', [
-      id,
-    ]);
+    const rental = await db.query("SELECT * FROM rentals WHERE id=$1", [id]);
+
     if (!rental.rowCount) return res.sendStatus(404);
     if (rental.rows[0].returnDate) return res.sendStatus(400);
     next();
@@ -39,7 +38,7 @@ export async function rentalsAttValidate(req, res, next) {
 export async function rentalsDeleteValidate(req, res, next) {
   const { id } = req.params;
   try {
-    const rental = await db.query(`SELECT * FROM rentals WHERE id = $1;`, [id]);
+    const rental = await db.query(`SELECT * FROM rentals WHERE id=$1;`, [id]);
     if (!rental.rowCount) return res.sendStatus(404);
     if (!rental.rows[0].returnDate) return res.sendStatus(400);
     next();
