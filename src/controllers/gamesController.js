3969acaa7455771b8eb3b65b9423f87d97b8ad1a
breno-aredes <breno_mg10@hotmail.com>
2023-02-12 22:19:54 -0300

    feat: game list created

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -1,5 +1,12 @@
 import { db } from "../database/database.connection.js";
 
-export async function listGames(req, res) {}
+export async function listGames(req, res) {
+  try {
+    const games = await db.query("SELECT * FROM games;");
+    res.send(games.rows);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
 
 export async function newGames(req, res) {}
