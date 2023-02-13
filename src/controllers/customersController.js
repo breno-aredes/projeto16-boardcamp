7fc27845fb3b5e9d5996677e47af46dade540d6c
breno-aredes <breno_mg10@hotmail.com>
2023-02-13 10:05:55 -0300

    feat: list customer by id

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -9,6 +9,20 @@ export async function listCustomers(req, res) {
   }
 }
 
+export async function listCustomersById(req, res) {
+  const { id } = req.params;
+  try {
+    const customer = await database.query(
+      `SELECT * FROM customers WHERE id = $1;`,
+      [id]
+    );
+    if (!customer.rowCount) return res.sendStatus(404);
+    res.send(customer.row[0]);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
 export async function newCustomers(req, res) {}
 
 export async function attCustomers(req, res) {}
--- src/routes/customers.Routes.js
+++ src/routes/customers.Routes.js
@@ -3,10 +3,12 @@ import {
   newCustomers,
   listCustomers,
   attCustomers,
+  listCustomersById,
 } from "../controllers/customersController.js";
 
 export const customersRouter = Router();
 
 customersRouter.get("/customoers", listCustomers);
+customersRouter.get("/customers/:id?", listCustomersById);
 customersRouter.post("/customers", newCustomers);
 customersRouter.put("/customers", attCustomers);
